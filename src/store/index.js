545fe776cba58cf4b08b4d394f24ee0a85e32aca
Faig-Abaszada <faik.abaszada@gmail.com>
2021-09-11 23:02:26 +0400

    #maraqli - createObjectURL

--- src/store/index.js
+++ src/store/index.js
@@ -38,6 +38,12 @@ export default new Vuex.Store({
     updateBlogTitle(state, payload){
       state.blogTitle = payload;
     },
+    fileNameChange(state, payload){
+      this.blogPhotoName = payload;
+    },
+    createFileURL(state,payload) {
+      state.blogPhotoFileURL = payload;
+    },
     toggleEditPost(state, payload) {
       state.editPost = payload;
       console.log(state.editPost);
