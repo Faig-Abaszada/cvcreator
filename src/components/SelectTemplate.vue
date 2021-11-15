7b390265fa7c5143033d5e3ac0d0ac2a074a39b5
Faig-Abaszada <faik.abaszada@gmail.com>
2021-11-15 15:39:26 +0400

    refactor - select Template functions coded, BUT! - after change template theme page don t show it in SelectTemplate

--- src/components/SelectTemplate.vue
+++ src/components/SelectTemplate.vue
@@ -1,22 +1,151 @@
 <template>
-<div class="s-temp">
-  <h1>This is template select theme</h1>
-</div>
+  <div class="wrapper s-p">
+    <div class="overlayed">
+      <header>
+        <a href="#">Back to editor</a>
+        <div class="colors">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="btn-actions">
+<!--          <button class="download">Download PDF</button>-->
+<!--          <button class="more-action">...</button>-->
+        </div>
+      </header>
+      <div class="container">
+        <div class="row ">
+          <div class="col-3 ">
+            <div class="row templates">
+              <div class="col-lg-6">
+                <div class="template" @click="updateTemplateName('Sherlock')">
+                  <h2>Sherlock</h2>
+                  <img  :src='require("@/assets/templates/cvform.webp")'>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="template" @click="updateTemplateName('BasicTheme')">
+                  <h2>BasicTheme</h2>
+                  <img  :src='require("@/assets/templates/cvform2.webp")'>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-9">
+            <ResumePreview class="preview"  :resume="this.resume"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
+import ResumePreview from "./ResumePreview";
+import {mapFields} from  "vuex-map-fields";
+
+import 'firebase/storage';
+import db from '../firebase/firebaseInit';
+
 export default {
-  name: "SelectTemplate"
+  name: "SelectTemplate",
+  components: {
+    ResumePreview
+  },
+  computed: {
+    ...mapFields([
+      'resume'
+    ])
+  },
+  methods: {
+    async updateTemplateName(templateName) {
+      const resumeDB = await db.collection('resumes').doc(this.$route.params.resumeid);
+
+      await resumeDB.update({
+        templateName: templateName
+      });
+      this.$emit('updatedTemplateName');
+
+    },
+
+  }
+
 }
 </script>
 
-<style scoped>
-.s-temp {
-  background-color: #000;
+<style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .colors {
+    display: flex;
+
+    * {
+      height: 32px;
+      width: 32px;
+      background-color: #1eb8b8;
+      border-radius: 50%;
+    }
+  }
+}
+.template {
+  &:hover {
+    cursor: pointer;
+  }
+
+  h2 {
+    font-size: 16px;
+    text-align: center;
+    color: #fff;
+  }
+
+  img {
+    width: 100%;
+  }
+}
+
+.wrapper {
+  background-color: rgb(34, 34, 34,);
   position: fixed;
   top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 101;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  //width: 100%;
+  //height: auto;
+  z-index: 110;
+  display: flex;
+  flex-direction: column;
 }
+.overlayed {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: block;
+}
+
+.templates {
+  overflow-y: scroll !important;
+  height: 100vh;
+  position: initial;
+}
+.preview {
+  position: initial;
+  width: 100%;
+  overflow-y: scroll !important;
+
+  .resume-doc {
+    transform: scale(0.9) !important;
+  }
+}
+
+
 </style>
